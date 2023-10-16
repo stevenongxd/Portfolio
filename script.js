1d19311a871f7957b1533f827c30c3c098fdcfba
Steven Ong <song24112002@gmail.com>
2023-10-16 19:23:46 +0700

    Fade Effect Fix

--- script.js
+++ script.js
@@ -7,69 +7,46 @@ const portfolioLink = document.querySelector('a[href="#portfolio"]');
 const portfolioSection = document.querySelector('#portfolio');
 const contactLink = document.querySelector('a[href="#contact"]');
 const contactSection = document.querySelector('#contact');
-
-homeLink.addEventListener('click', () => {
-    // Fade out the home section
-    homeSection.style.opacity = 0;
-
-    // After a brief delay, fade it back in
-    setTimeout(() => {
-    homeSection.style.opacity = 1;
-    }, 500); // Adjust the time to match fade duration
-
-    // Fade out the resume section
-    resumeSection.style.opacity = 0;
-
-    // Fade out the portfolio section
-    portfolioSection.style.opacity = 0;
-});
-
-resumeLink.addEventListener('click', () => {
-    resumeSection.style.opacity = 0;
-
-    setTimeout(() => {
-    resumeSection.style.opacity = 1;
-    }, 500);
-
-    homeSection.style.opacity = 0;
-
-    portfolioSection.style.opacity = 0;
-});
-
-portfolioLink.addEventListener('click', () => {
-    portfolioSection.style.opacity = 0;
-
-    setTimeout(() => {
-    portfolioSection.style.opacity = 1;
-    }, 500);
-
-    homeSection.style.opacity = 0;
-
-    resumeSection.style.opacity = 0;
-});
-
-contactLink.addEventListener('click', () => {
-    contactSection.style.opacity = 0;
-    setTimeout(() => {
-    contactSection.style.opacity = 1;
-    }, 500);
-
-    homeSection.style.opacity = 0;
-
-    resumeSection.style.opacity = 0;
-
-    portfolioSection.style.opacity = 0;
-});
-
-// Fade toggle for about section
 const aboutLink2 = document.querySelector('a[href="#about"]');
 const aboutSection = document.querySelector('#about');
+
+homeLink.addEventListener('click', () => {
+    homeSection.style.opacity = 0;
+
+    setTimeout(() => {
+        homeSection.style.opacity = 1;
+    }, 500);
+});
 
 aboutLink2.addEventListener('click', () => {
     aboutSection.style.opacity = 0;
 
     setTimeout(() => {
         aboutSection.style.opacity = 1;
+    }, 500);
+});
+
+resumeLink.addEventListener('click', () => {
+    resumeSection.style.opacity = 0;
+
+    setTimeout(() => {
+        resumeSection.style.opacity = 1;
+    }, 500);
+});
+
+portfolioLink.addEventListener('click', () => {
+    portfolioSection.style.opacity = 0;
+
+    setTimeout(() => {
+        portfolioSection.style.opacity = 1;
+    }, 500);
+});
+
+contactLink.addEventListener('click', () => {
+    contactSection.style.opacity = 0;
+
+    setTimeout(() => {
+        contactSection.style.opacity = 1;
     }, 500);
 });
 
